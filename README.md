9d025c88a8d31b5d42c981bd97f1d4d1f31be8c6
jay-yadavdws <jay.yadavdws@gmail.com>
2024-08-04 09:31:55 +0530

    Adding notes file in docx for better understanding and configuring user using InMemoryUserDetailsManager.java

---
+++ README.md
@@ -0,0 +1,5 @@
+### Spring Method Security With Roles & Permission
+
+* Key Considerations :
+
+We have configured 2 USERS for the application using `@Bean UserDetailsService` and its `InMemoryUserDetailsManager.java` implementation
--- src/main/java/dev/lynx/secure/method/config/SecurityConfig.java
+++ src/main/java/dev/lynx/secure/method/config/SecurityConfig.java
@@ -3,34 +3,46 @@ package dev.lynx.secure.method.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity(debug = false)
 @EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
-    //@Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.sessionManagement(sessionManagementConfigurer -> sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        return httpSecurity.build();
+   /* @Bean
+    public UserDetailsService userDetailsService() {
+        User user1 = new User("user1", "{noop}user1", List.of(new SimpleGrantedAuthority("ROLE_USER")));  // Granted Authority will always have ROLE_ prefix.
+        User user2 = new User("admin1", "{noop}admin1", List.of(new SimpleGrantedAuthority("ROLE_ADMIN")));
+        UserDetails[] userDetails = {user1, user2};
+        return new InMemoryUserDetailsManager(userDetails);
+    }*/
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+//        new DelegatingPasswordEncoder();
+        // pls also check DelegatingPasswordEncoder class for more info.
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();  // BASED ON PREFIX {noop}, {bcrypt} AUTOMATICALLY USES APPROPRIATE ENCODER
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
-        User user1 = new User("user1", "{noop}user1", List.of(new SimpleGrantedAuthority("ROLE_USER")));
-        User user2 = new User("admin1", "{noop}admin1", List.of(new SimpleGrantedAuthority("ROLE_ADMIN")));
-        UserDetails[] userDetails = {user1, user2};
-        return new InMemoryUserDetailsManager(userDetails);
+        UserDetails user1 = User.builder()
+                .username("admin1").password("{noop}admin1").roles("ADMIN")  // WHEN USING BUILDER IT WILL APPEND PREFIX ROLE_
+                .build();
+        UserDetails user2 = User.builder()
+                .username("user1").password("{bcrypt}$2a$10$9rqj.ubsLGiy8YSrmkD.HefAAGghlKoybkoFO56i7DQJsC3XDiWFu").roles("USER") //pwd is user1
+                .build();
+
+        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+        inMemoryUserDetailsManager.createUser(user1);
+        inMemoryUserDetailsManager.createUser(user2);
+        return inMemoryUserDetailsManager;
     }
 }
